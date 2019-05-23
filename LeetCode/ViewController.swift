665893a80c3e84c452ece6d4d56e391efcd658ff
Qingpu Shi <qish@microsoft.com>
2019-05-23 10:43:44 +0800

    Initial Commit

---
+++ LeetCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by Qingpu Shi on 2019/5/23.
+//  Copyright © 2019 Qingpu Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
