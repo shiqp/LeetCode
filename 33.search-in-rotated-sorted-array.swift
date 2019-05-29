5ecae4c1fa73f148be9cbc47b06d39a3dc81408e
Qingpu Shi <qish@microsoft.com>
2019-05-29 15:05:24 +0800

    33.search-in-rotated-sorted-array

---
+++ 33.search-in-rotated-sorted-array.swift
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=33 lang=swift
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var low = 0
+        var high = nums.endIndex - 1
+        while low <= high {
+            let mid = (low + high) / 2
+            if nums[mid] == target {
+                return mid
+            }
+
+            if nums[low] < nums[high] {
+                if nums[mid] < target {
+                    low = mid + 1
+                } else {
+                    high = mid - 1
+                }
+            } else {
+                if nums[mid] > nums[high] {
+                    if nums[low] <= target && target < nums[mid] {
+                        high = mid - 1
+                    } else {
+                        low = mid + 1
+                    }
+                } else {
+                    if nums[mid] < target && target <= nums[high] {
+                        low = mid + 1
+                    } else {
+                        high = mid - 1
+                    }
+                }
+            }
+        }
+        
+        return -1
+    }
+}
+
--- LeetCode/BinarySearch.swift
+++ LeetCode/BinarySearch.swift
@@ -101,4 +101,49 @@ class BinarySearch: NSObject {
         return result > Int32.max ? Int(Int32.max) : Int(result)
     }
 
+    /// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+    /// (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+    /// You are given a target value to search. If found in the array return its index, otherwise return -1.
+    /// You may assume no duplicate exists in the array.
+    /// Your algorithm's runtime complexity must be in the order of O(log n).
+    ///
+    /// - Parameters:
+    ///   - nums: The nums array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+    ///   - target: The target.
+    /// - Returns: If found in the array return its index, otherwise return -1.
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var low = 0
+        var high = nums.endIndex - 1
+        while low <= high {
+            let mid = (low + high) / 2
+            if nums[mid] == target {
+                return mid
+            }
+
+            if nums[low] < nums[high] {
+                if nums[mid] < target {
+                    low = mid + 1
+                } else {
+                    high = mid - 1
+                }
+            } else {
+                if nums[mid] > nums[high] {
+                    if nums[low] <= target && target < nums[mid] {
+                        high = mid - 1
+                    } else {
+                        low = mid + 1
+                    }
+                } else {
+                    if nums[mid] < target && target <= nums[high] {
+                        low = mid + 1
+                    } else {
+                        high = mid - 1
+                    }
+                }
+            }
+        }
+
+        return -1
+    }
+
 }
