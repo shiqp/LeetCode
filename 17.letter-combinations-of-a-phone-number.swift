169c4d32b23a5472ae91438998bdef89f08b170a
Qingpu Shi <qish@microsoft.com>
2019-09-18 18:05:51 +0800

    [17] Letter Combinations of a Phone Number

---
+++ 17.letter-combinations-of-a-phone-number.swift
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=17 lang=swift
+ *
+ * [17] Letter Combinations of a Phone Number
+ */
+class Solution {
+    let symbols = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+
+    func letterCombinations(_ digits: String) -> [String] {
+        if digits.count == 0 {
+            return [String]()
+        }
+
+        let index = digits.index(after: digits.startIndex)
+        var combinations = letterCombinations(String(digits[index...]))
+        let symbol = symbols[Int(digits.first!.asciiValue!) - 48]
+
+        if combinations.count == 0 {
+            combinations.append("")
+        }
+
+        var result = [String]()
+        for comb in combinations {
+            for character in symbol {
+                result.append("\(character)\(comb)")
+            }
+        }
+
+        return result
+    }
+}
+
