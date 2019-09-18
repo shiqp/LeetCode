43c8419cd40c4b1347dc2f84e87022918e3abad0
Qingpu Shi <qish@microsoft.com>
2019-09-18 18:33:59 +0800

    [19] Remove Nth Node From End of List

---
+++ 19.remove-nth-node-from-end-of-list.swift
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=19 lang=swift
+ *
+ * [19] Remove Nth Node From End of List
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class Solution {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        let node = ListNode(0)
+        node.next = head
+
+        var p: ListNode? = node, q: ListNode? = node
+        
+        var n = n
+        while p != nil {
+            p = p?.next
+            if n >= 0 {
+                n -= 1
+            } else {
+                q = q?.next
+            }
+        }
+
+        q?.next = q?.next?.next
+
+        return node.next
+    }
+}
+
---
+++ LeetCode/List/ListNode.swift
@@ -0,0 +1,18 @@
+//
+//  ListNode.swift
+//  LeetCode
+//
+//  Created by Qingpu Shi on 2019/9/18.
+//  Copyright © 2019 Qingpu Shi. All rights reserved.
+//
+
+import Foundation
+
+public class ListNode {
+     public var val: Int
+     public var next: ListNode?
+     public init(_ val: Int) {
+         self.val = val
+         self.next = nil
+     }
+}
